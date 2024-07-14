63abfa3fcc95c111b39d9e960f0aca2280a0ce62
Reza <karimireza77@yahoo.com>
2024-07-14 10:10:10 +0000

    senario 6

--- a.sh
+++ a.sh
@@ -20,4 +20,34 @@ git add .
 git commit -m "this is Senario 2"
 
 # Senario 3
+touch app.py
+echo "print('reza')" > app.py
+git add .
+git commit -m "this is app.py"
+git blame app.py
+or
+# this show who changed the app and what was he/she changed
+git blame | git show
 
+
+# Senario 4
+# preview
+git clean -n -d
+#remove
+git clean -f -d
+
+# Senario 5
+# generate ssh
+ssh-keygen -t ed25519 -C karimieza77@yahoo.com
+# add ssh key to agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+# copy the ssh key
+cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
+# do things on git hub
+# for test
+ssh -T git@github.com
+
+# Senario 6
+git checkout feature_branch
+git push 
