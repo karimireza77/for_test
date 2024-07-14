e81d45065511448645fe948d50b201386196d795
Reza <karimireza77@yahoo.com>
2024-07-14 09:09:48 +0000

    Senario 2

---
+++ a.sh
@@ -0,0 +1,18 @@
+# Filoger MLOps School
+# EX6
+# Reza Karimi
+
+# Senario 1
+mkdir mlops_ex06
+cd mloops_ex06
+git init
+git config --global user.name Reza
+git config --global user.email karimireza77@yahoo.com
+echo "HI TA, this is my project" > Readme.md
+git add .
+git commit -m "Senario 1"
+
+# Senario 2
+git checkout -b feature-branch
+git branch
+
